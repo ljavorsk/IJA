ff8f7b6b689b9e397c3246d9fbfebb28bab2bf41
Lukas Javorsky <ljavorsk@redhat.com>
2020-03-31 00:22:13 +0200

    Add file hierarchy in second homework

---
+++ ukol2/ija/ija2019/homework2/maps/Line.java
@@ -0,0 +1,25 @@
+/*
+ * Zdrojové kódy jsou součástí zadání 2. úkolu pro předmětu IJA v ak. roce 2019/2020.
+ * (C) Lukas Javorsky
+ */
+package ija.ija2019.homework2.maps;
+
+/**
+ * Reprezentuje linku autobusovej prevadzky
+ * @author xjavor20
+ */
+public interface Line {
+
+    /**
+     * Vrací hodnotu souřadnice x.
+     * @return Souřadnice x.
+     */
+    public int getX();
+    
+    /**
+     * Vrací hodnotu souřadnice y.
+     * @return Souřadnice y.
+     */
+    public int getY();
+    
+}
---
+++ ukol2/ija/ija2019/homework2/maps/MyCoordinate.java
@@ -0,0 +1,59 @@
+/**
+ * @author xjavor20
+ */
+
+package ija.ija2019.homework2.maps;
+
+import ija.ija2019.homework2.maps.Coordinate;
+
+public class MyCoordinate implements Coordinate{
+
+    private int x_coordinate;
+    private int y_coordinate;
+
+    /**
+     * Constructor
+     */
+    private MyCoordinate(int x_coordinate, int y_coordinate){
+        this.x_coordinate = x_coordinate;
+        this.y_coordinate = y_coordinate;
+    }
+
+    
+    public static MyCoordinate create(int x, int y){
+        if (x >= 0 && y >= 0){
+            return new MyCoordinate(x, y);
+        }
+        return null;
+    }
+
+    @Override
+    public int getX(){
+        return this.x_coordinate;
+    }
+
+    @Override
+    public int getY(){
+        return this.y_coordinate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){ // Test for identity
+            return true;
+        }
+        if (!(obj instanceof MyCoordinate)){ // Test before casting
+            return false;
+        }
+        MyCoordinate my_coord_obj = (MyCoordinate) obj; // Casting
+        return (my_coord_obj.getX() == (this.getX()) && my_coord_obj.getY() == (this.getY()));
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 1;
+        hash = hash * 42 + this.getX();
+        hash = hash * 42 + this.getY();
+        return hash;
+    }
+}
